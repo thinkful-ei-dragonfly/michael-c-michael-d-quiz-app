8fbe3f134730031a1c26f9129f30deda160544d1
m-cal <iengineersoftware@gmail.com>
2019-05-14 17:00:17 -0400

    changing pairs

---
+++ src/Question.js
@@ -0,0 +1,18 @@
+// import TriviaApi from './TriviaApi';
+
+class Question {
+  constructor() {
+    this.category = '';
+    this.correct_answer = '';
+    this.difficulty = '';
+    this.incorrect_answers = [];
+    this.question = '';
+    this.type = '';
+  }
+
+}
+
+
+
+
+export default Question;
